9de631f7bb0fc9e87a2dbc5b2d49913705be15b2
¨inv1tro¨ <funky.odisey@gmail.com>
2022-07-04 20:54:12 -0300

    primer trabajo js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+let option = prompt('Elija un codigo de descuento:
+        1: Descuento redes sociales
+        
+        2: Descuento cliente
+
+        switch (option) {
+
+            case "1"
+            console.log(Descuento redes sociales)
+            case "2"
+            console.log (Descuento cliente )
+
+
+
+
+        }
+    
+
+
+
+
+
+        ');
