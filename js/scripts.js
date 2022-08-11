9e495d339c8597d455f1a542388da2db6ea02115
¨inv1tro¨ <funky.odisey@gmail.com>
2022-08-11 08:02:38 -0300

    forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
-let nombre = prompt("Cual es tu nombre")
-let apellido = prompt("Cual es tu apellido")
-let bienvenido = (nombre, apellido) => alert(`Bienvenido ${nombre} ${apellido}`); // funcion flecha != hoisting function declarada
+// let nombre = prompt("Cual es tu nombre")
+// let apellido = prompt("Cual es tu apellido")
+// let bienvenido = (nombre, apellido) => alert(`Bienvenido ${nombre} ${apellido}`); // funcion flecha != hoisting function declarada
 
-bienvenido(nombre, apellido);
+// bienvenido(nombre, apellido);
 
 
 
-// switch es igual al IF  pero mas dinamico // default = else 
-let elijeTuProducto = parseInt(prompt(`Elija tu producto
-1- Mochila1 $3500
-2- mochila2 $2000
-3- mochila3 $20000`));
-switch (elijeTuProducto) {
-    case 1:
-        alert(`Elegiste Mochila1`)
+// // switch es igual al IF  pero mas dinamico // default = else 
+// let elijeTuProducto = parseInt(prompt(`Elija tu producto
+// 1- Mochila1 $3500
+// 2- mochila2 $2000
+// 3- mochila3 $20000`));
+// switch (elijeTuProducto) {
+//     case 1:
+//         alert(`Elegiste Mochila1`)
 
-        break;
-    case 2:
-        alert("Elegiste mochila2")
-        break;
+//         break;
+//     case 2:
+//         alert("Elegiste mochila2")
+//         break;
 
-    case 3:
-        alert("Elegiste  mochila3")
-        break;
+//     case 3:
+//         alert("Elegiste  mochila3")
+//         break;
 
-    default:
-        alert("Ingrese un caracter valido - Solo numeros")
-        break;
-}
+//     default:
+//         alert("Ingrese un caracter valido - Solo numeros")
+//         break;
+// }
 
 // // arreglos  // iterando arreglos =  recorrer el arreglo, posicionamiento, iteracion. 
 // let listadoMochilas = ["mochila1", "mochila2", "mochila3"]
@@ -47,15 +47,26 @@ switch (elijeTuProducto) {
 
 // Objetos y funcion constructora 
 
-function mochila (marca, precio, disponiblidad) {
+function Mochila (marca, precio, disponiblidad) {
     this.marca = marca;
     this.precio = precio;
     this.disponiblidad = disponiblidad;
 }
 
-let mochila1 = new mochila ("Nike",3500,true);
-let mochila2 = new mochila ("Puma",4000,true);
-let mochila3 = new mochila ("Adidas",3000,true);
+const Mochila1 = new Mochila ("Nike",3500,true);
+const Mochila2 = new Mochila ("Puma",4000,true);
+const Mochila3 = new Mochila ("Adidas",3000,true);
 
 let listadoDeMochilas = []
-listadoDeMochilas.push (mochila1,mochila2,mochila3)
+listadoDeMochilas.push (Mochila1,Mochila2,Mochila3)
+
+let carrito = [] 
+
+carrito.push (listadoDeMochilas)
+
+
+function listarArray (arr) {
+  arr.forEach (e=> console.log (e))
+}
+
+listarArray(carrito)
